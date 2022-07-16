4c00364844b882b018501f8261c6f7410c4e3aa1
maitian352 <68207888+maitian352@users.noreply.github.com>
2022-07-16 14:09:03 -0400

    Update drive.py

--- Program/IO/drive.py
+++ Program/IO/drive.py
@@ -61,3 +61,17 @@ def steer(steering):
 def throttle(throttle):
     global targetThrottle
     targetThrottle = max(0, min(throttle, 100))
+
+    from PIL import Image
+import numpy
+import math
+
+apple = Image.open("apple.png")
+array = numpy.array(apple)
+for i in range(len(array)):
+    for j in range(len(array[i])):
+        if array[i][j][0] > 150:
+            array[i][j] = [0,min(255,10 * array[i][j][1]),min(255,10 * array[i][j][2]),255]
+
+apple2 = Image.fromarray(array)
+apple2.show()
