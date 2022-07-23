b1c67efb42d8b9ace21d49111f6b1f4e643ecfe7
maitian352 <68207888+maitian352@users.noreply.github.com>
2022-07-23 15:46:00 -0400

    the E
    EEEE
    camera

--- Program/IO/camera.py
+++ Program/IO/camera.py
@@ -3,7 +3,7 @@ import time
 from PIL import Image
 import datetime
 
-camera = CSICamera(width=224, height=224, capture_width=1080, capture_height=720, capture_fps=30)
+camera = CSICamera(width=1080, height=720, capture_width=1080, capture_height=720, capture_fps=30)
 camera.running = True
 
 index = 1
