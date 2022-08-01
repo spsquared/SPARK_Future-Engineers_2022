0293cc85ddcc46b8092f6cdceb71fcb6f88f9405
maitian352 <68207888+maitian352@users.noreply.github.com>
2022-07-31 20:39:07 -0400

    Update filter.py

--- Program/CV/filter.py
+++ Program/CV/filter.py
@@ -29,41 +29,69 @@ def filter(imgIn: numpy.ndarray):
 
 def predict(imgIn: numpy.ndarray):
     global redMax, redMin, greenMax, greenMin, wallMax, wallMin
-    params = cv2.SimpleBlobDetector_Params()
-    params.minThreshold = 30
-    params.maxThreshold = 255
-    blobs = cv2.SimpleBlobDetector_create(params)
-    blobs.empty()
-    rMask = cv2.inRange(imgIn, redMin, redMax)
-    gMask = cv2.inRange(imgIn, greenMin, greenMax)
-    wMask = cv2.inRange(imgIn, wallMin, wallMax)
-    rImg = cv2.medianBlur(rMask, 5)
-    gImg = cv2.medianBlur(gMask, 5)
-    wImg = cv2.medianBlur(wMask, 5)
-    rKps = blobs.detect(rImg)
-    gKps = blobs.detect(gImg)
-    wKps = blobs.detect(wImg)
-    croppedWImg = wImg[45:100,130:143]
-    wallHeights = numpy.count_nonzero(croppedWImg > 1,axis=0)
-    wallHeights2 = []
-    for i in range(len(wallHeights)):
-        if wallHeights[i] != 0:
-            wallHeights2.append(wallHeights[i])
-    if len(wallHeights2) == 0:
-        wallHeight = 0
-    else:
-        wallHeight = statistics.median(wallHeights2)
-    # -100 = turn left a lot
-    # 100 = turn right a lot
-    for i in range(len(rKps)):
-        if 131 < rKps[i].pt[0] * 5 / 12 + rKps[i].pt[1] + rKps[i].sizeof:
+    try:
+        params = cv2.SimpleBlobDetector_Params()
+        params.minThreshold = 30
+        params.maxThreshold = 255
+        blobs = cv2.SimpleBlobDetector_create(params)
+        blobs.empty()
+        rMask = cv2.inRange(imgIn, redMin, redMax)
+        gMask = cv2.inRange(imgIn, greenMin, greenMax)
+        wMask = cv2.inRange(imgIn, wallMin, wallMax)
+        rImg = cv2.medianBlur(rMask, 5)
+        gImg = cv2.medianBlur(gMask, 5)
+        wImg = cv2.medianBlur(wMask, 5)
+        rKps = blobs.detect(rImg)
+        gKps = blobs.detect(gImg)
+        wKps = blobs.detect(wImg)
+        croppedWImgLeft = wImg[45:100,20:35]
+        croppedWImgCenter = wImg[45:100,130:143]
+        croppedWImgRight = wImg[45:100,237:252]
+        wallHeightsLeft = numpy.count_nonzero(croppedWImgLeft > 1,axis=0)
+        wallHeights2Left = []
+        for i in range(len(wallHeightsLeft)):
+            if wallHeightsLeft[i] != 0:
+                wallHeights2Left.append(wallHeightsLeft[i])
+        if len(wallHeights2Left) == 0:
+            wallHeightLeft = 0
+        else:
+            wallHeightLeft = statistics.median(wallHeights2Left)
+        wallHeightsCenter = numpy.count_nonzero(croppedWImgCenter > 1,axis=0)
+        wallHeights2Center = []
+        for i in range(len(wallHeightsCenter)):
+            if wallHeightsCenter[i] != 0:
+                wallHeights2Center.append(wallHeightsCenter[i])
+        if len(wallHeights2Center) == 0:
+            wallHeightCenter = 0
+        else:
+            wallHeightCenter = statistics.median(wallHeights2Center)
+        wallHeightsRight = numpy.count_nonzero(croppedWImgRight > 1,axis=0)
+        wallHeights2Right = []
+        for i in range(len(wallHeightsRight)):
+            if wallHeightsRight[i] != 0:
+                wallHeights2Right.append(wallHeightsRight[i])
+        if len(wallHeights2Right) == 0:
+            wallHeightRight = 0
+        else:
+            wallHeightRight = statistics.median(wallHeights2Right)
+        # -100 = turn left a lot
+        # 100 = turn right a lot
+        for i in range(len(rKps)):
+            if 131 < rKps[i].pt[0] * 5 / 12 + rKps[i].pt[1] + rKps[i].sizeof * 2:
+                return -100
+        for i in range(len(gKps)):
+            if 131 < (272 - gKps[i].pt[0]) * 5 / 12 + gKps[i].pt[1] + gKps[i].sizeof * 2:
+                return 100
+        if wallHeightCenter > 25:
             return -100
-    for i in range(len(gKps)):
-        if 131 < (272 - gKps[i].pt[0]) * 5 / 12 + gKps[i].pt[1] + gKps[i].sizeof:
-            return 100
-    if wallHeight > 23:
-        return -100
-    return 0
+        if wallHeightRight > 25:
+            return -100
+        if wallHeightLeft > 25:
+            return 50
+        return 0
+    except Exception as err:
+        print(err)
+        io.error()
 
 def setColors(data):
     global redMax, redMin, greenMax, greenMin, wallMax, wallMin
