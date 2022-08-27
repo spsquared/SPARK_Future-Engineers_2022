0f33419dc802617e5c3c791d6ce1c579f4a6cf88
Radioactive64 <radioactivestuff.101@gmail.com>
2022-08-27 18:25:26 -0400

    c o l o r s

--- SPARK-Randomiser/index.js
+++ SPARK-Randomiser/index.js
@@ -48,72 +48,72 @@ var drawPillarCircle = function(x,y){
     ctx.stroke();
 }
 var drawStartingPosition = function(rotations,position,direction){
-    ctx.fillStyle = 'rgba(255,0,0,0.5)';
+    ctx.fillStyle = 'rgba(255,255,0,0.5)';
     ctx.translate(155,155);
     ctx.rotate(0.5 * Math.PI * rotations);
     if(position === 0){
         ctx.fillRect(-50,-150,50,40);
         if(direction === 0){
-            ctx.fillStyle = 'rgba(0,255,255,1)';
+            ctx.fillStyle = 'rgba(0,255,255,0.5)';
             ctx.fillRect(-50,-150,10,40);
         }
         else{
-            ctx.fillStyle = 'rgba(0,255,255,1)';
+            ctx.fillStyle = 'rgba(0,255,255,0.5)';
             ctx.fillRect(-10,-150,10,40);
         }
     }
     else if(position === 1){
         ctx.fillRect(0,-150,50,40);
         if(direction === 0){
-            ctx.fillStyle = 'rgba(0,255,255,1)';
+            ctx.fillStyle = 'rgba(0,255,255,0.5)';
             ctx.fillRect(0,-150,10,40);
         }
         else{
-            ctx.fillStyle = 'rgba(0,255,255,1)';
+            ctx.fillStyle = 'rgba(0,255,255,0.5)';
             ctx.fillRect(40,-150,10,40);
         }
     }
     else if(position === 2){
         ctx.fillRect(-50,-110,50,20);
         if(direction === 0){
-            ctx.fillStyle = 'rgba(0,255,255,1)';
+            ctx.fillStyle = 'rgba(0,255,255,0.5)';
             ctx.fillRect(-50,-110,10,20);
         }
         else{
-            ctx.fillStyle = 'rgba(0,255,255,1)';
+            ctx.fillStyle = 'rgba(0,255,255,0.5)';
             ctx.fillRect(-10,-110,10,20);
         }
     }
     else if(position === 3){
         ctx.fillRect(0,-110,50,20);
         if(direction === 0){
-            ctx.fillStyle = 'rgba(0,255,255,1)';
+            ctx.fillStyle = 'rgba(0,255,255,0.5)';
             ctx.fillRect(0,-110,10,20);
         }
         else{
-            ctx.fillStyle = 'rgba(0,255,255,1)';
+            ctx.fillStyle = 'rgba(0,255,255,0.5)';
             ctx.fillRect(40,-110,10,20);
         }
     }
     else if(position === 4){
         ctx.fillRect(-50,-90,50,40);
         if(direction === 0){
-            ctx.fillStyle = 'rgba(0,255,255,1)';
+            ctx.fillStyle = 'rgba(0,255,255,0.5)';
             ctx.fillRect(-50,-90,10,40);
         }
         else{
-            ctx.fillStyle = 'rgba(0,255,255,1)';
+            ctx.fillStyle = 'rgba(0,255,255,0.5)';
             ctx.fillRect(-10,-90,10,40);
         }
     }
     else if(position === 5){
         ctx.fillRect(0,-90,50,40);
         if(direction === 0){
-            ctx.fillStyle = 'rgba(0,255,255,1)';
+            ctx.fillStyle = 'rgba(0,255,255,0.5)';
             ctx.fillRect(0,-90,10,40);
         }
         else{
-            ctx.fillStyle = 'rgba(0,255,255,1)';
+            ctx.fillStyle = 'rgba(0,255,255,0.5)';
             ctx.fillRect(40,-90,10,40);
         }
     }
